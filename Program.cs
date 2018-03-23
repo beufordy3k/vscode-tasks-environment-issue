5c048fdba42ada06db3ad9bc3f97eb2860ba0d68
Rufus <rufushy3k@gmail.com>
2018-03-23 14:53:34 -0400

    Initial Commit Reproducing Tasks Environment Variable Bug

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vscode_tasks_environment_issue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
